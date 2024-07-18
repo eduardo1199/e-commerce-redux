22863c6e32131d21b943c13f78a7b496bfc6b3df
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-07-18 09:22:44 -0300

    feat: create login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -72,6 +74,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
